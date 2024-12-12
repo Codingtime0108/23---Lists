66c174016bee0cd30f1c6bb0b0f953f0d345c94d
Codingtime <13tia.vora@dulwich-seoul.kr>
2024-12-12 18:40:31 +0900

    lists

---
+++ lists.py
@@ -0,0 +1,12 @@
+my_list = []
+print()
+
+numbers = [10,20,30,40,50]
+print(numbers)
+
+triples = [1,2,3] * 3
+print(triples)
+
+aList = [100,200,300,400,500]
+aList = aList[::-1]
+print(aList,"\n")
